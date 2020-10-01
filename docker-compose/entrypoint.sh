c3f96efa27ec46049b39097dc603ea1292d5121c
Radu Neda <radu.neda@tremend.com>
2020-10-01 16:32:02 +0300

    Updated docker-compose first example

--- docker-compose/entrypoint.sh
+++ docker-compose/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Login gcloud cli with service account file
-gcloud auth activate-service-account --key-file ${TF_VAR_sa_terraform}
+gcloud auth activate-service-account --key-file ${GOOGLE_APPLICATION_CREDENTIALS}
 
 # Keep console interactive
 bash
